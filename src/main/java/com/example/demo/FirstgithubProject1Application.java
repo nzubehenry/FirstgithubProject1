1dac9f51e0240cf9d1d7095649c4e21de80c0bd8
IgweNzubeHenry <nzubehenry06@gmail.com>
2022-08-03 12:22:23 +0100

    first commit

---
+++ src/main/java/com/example/demo/FirstgithubProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgithubProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgithubProject1Application.class, args);
+	}
+
+}
